0f5bb672eabe4cc36186ce423752909c18f5d73f
Solw <113430055+SolwDawg@users.noreply.github.com>
2024-09-11 13:46:24 +0700

    fist

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MyApp()
-
-  );
+  return runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
           centerTitle: true,
-          title: Text('DiceApp cua Hieu'),
+          title: Text('DiceApp'),
           backgroundColor: Colors.teal,
         ),
         body: Dicepage(),
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 // class DicePage extends StatelessWidget {
 //     DicePage({Key? key}) : super(key: key);
 //
@@ -37,17 +38,15 @@ class Dicepage extends StatefulWidget {
 }
 
 class _DicepageState extends State<Dicepage> {
-  int leftDiceNumber =  1;
+  int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
-  void ChangeDiceValue(){
+  void ChangeDiceValue() {
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
       rightDiceNumber = Random().nextInt(6) + 1;
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -56,28 +55,24 @@ class _DicepageState extends State<Dicepage> {
         children: [
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: InkWell(
-                    onTap: (){
-                      ChangeDiceValue();
-                    },
-                    child: Image.asset("assets/dice$leftDiceNumber.png")),
-              )
-          ),
+            padding: const EdgeInsets.all(16.0),
+            child: InkWell(
+                onTap: () {
+                  ChangeDiceValue();
+                },
+                child: Image.asset("assets/dice$leftDiceNumber.png")),
+          )),
           Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: InkWell(
-                    onTap: (){
-                      ChangeDiceValue();
-                    },
-                    child: Image.asset("assets/dice$rightDiceNumber.png")),
-              )
-          )
+            padding: const EdgeInsets.all(16.0),
+            child: InkWell(
+                onTap: () {
+                  ChangeDiceValue();
+                },
+                child: Image.asset("assets/dice$rightDiceNumber.png")),
+          ))
         ],
       ),
     );
   }
 }
-
-
